5ae54c46aa398e0139e044c2f21fe21e07994394
Scott Brisson <scott.brisson@gmail.com>
2019-04-10 14:28:31 -0400

    Page size now affects partitions

--- ssd.py
+++ ssd.py
@@ -1,8 +1,20 @@
 import pandas
 import operator
 import sys
+import math
+from enum import Enum
 
 DEFAULT_TRACE_PATH = 'data/workloada_csv.csv'
+
+class page_size(Enum):
+    DEFAULT = 1
+    ONE_KB = 2
+    TWO_KB = 4
+    FOUR_KB = 8
+    EIGHT_KB = 16
+    SIXTEEN_KB = 32
+    THIRTY_TWO_KB = 64
+    SIXTY_FOUR_KB = 128
 
 def find_completed_writes(trace):
     writes = trace.loc[trace['operation'].str.contains('W')]
@@ -27,27 +39,40 @@ def compute_sector_write_counts(trace):
             sector_write_counts[sector] += 1 
     return sector_write_counts
 
-def compute_sector_partitions(trace):
+def compute_sector_partitions(trace, page_size):
     # get and sort sector write counts
     sector_counts = compute_sector_write_counts(trace)
     sorted_counts = sorted(sector_counts.items(), key=operator.itemgetter(1), reverse=True) # returns list of tuples
+
     # init vars for partition generation loop
-    max_count = sorted_counts[0][1]
+    max_count = 0
+    for i in range(page_size):
+        max_count += sorted_counts[i][1]
+
     current_max = max_count
     dead_sectors_index = -1
     partitions = [[]]
+
     # go through sectors from highest to lowest counts, creating partitions
     for i, tup in enumerate(sorted_counts):
-        count = tup[1]
+
+        # calculates the count based on page size
+        count = 0
+        for k in range(page_size):
+            count += sorted_counts[i+k][1]
+        i += page_size-1
+
         # if we hit the dead sectors, stop here
         if (count == 0):
             dead_sectors_index = i
             break
         # if we hit the max desired ratio, create a new partition
+
         if (current_max / count > 2):
             current_max = count
             partitions.append([])
         partitions[-1].append(tup)
+
     # give dead sectors their own partition
     dead_sectors = sorted_counts[dead_sectors_index:]
     partitions.append(dead_sectors)
@@ -59,9 +84,18 @@ def compute_sector_partitions(trace):
         size_str += str(len(p)) + ", "
     print(size_str)
 
+
+# def compute_minimum_frequency(trace, page_size, num_partitions):
+
+
 if __name__ == "__main__":
     trace_path = DEFAULT_TRACE_PATH
+    page_size = page_size.DEFAULT.value
+    # num_partitions = 2
+
     if (len(sys.argv) == 2):
         trace_path = sys.argv[1]
+
     trace = pandas.read_csv(trace_path)
-    compute_sector_partitions(trace)
+    compute_sector_partitions(trace, page_size)
+    # compute_minimum_frequency(trace, page_size, num_partitions)
