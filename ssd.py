f9522f95b1c8f89a66c38ff8b54a8bd7397bc777
bickybilly <bilbilv@mcmaster.ca>
2019-04-19 15:14:06 -0400

    Implemented binary search / bisection method style algorithm to find best ratio for fixed partition numbers

--- ssd.py
+++ ssd.py
@@ -44,6 +44,9 @@ def create_partitions(sorted_counts, max_count, page_size, target_ratio):
     current_max = max_count
     dead_sectors_index = -1
     partitions = [[]]
+    ratios = []
+
+    prev_count = max_count
 
     # go through page-size chunks of sectors from highest to lowest counts, creating partitions
     for i in range(0, len(sorted_counts), page_size):
@@ -93,7 +96,8 @@ def compute_sector_partitions(trace, page_size):
         size_str += str(len(p)) + ", "
     print(size_str)
 
-def compute_minimum_frequency(trace, page_size, max_num_partitions, min_target_ratio, step, max_iterations):
+
+def compute_minimum_frequency(trace, page_size, max_num_partitions, min_target_ratio, threshold, max_iterations):
     # get and sort sector write counts
     sector_counts = compute_sector_write_counts(trace)
     sorted_counts = sorted(sector_counts.items(), key=operator.itemgetter(1), reverse=True) # returns list of tuples
@@ -103,19 +107,41 @@ def compute_minimum_frequency(trace, page_size, max_num_partitions, min_target_r
     for i in range(page_size):
         max_count += sorted_counts[i][1]
 
+    lower_bound = 1
+    upper_bound = None
+    best_ratio = None
+    delta = math.inf
     target_ratio = min_target_ratio # starting point
-    num_partitions = float('inf')
+    num_partitions = math.inf
     iterations_taken = 0
 
     for i in range(max_iterations): # to make sure we don't go forever :-)
+        iterations_taken = i + 1
+        print(target_ratio)
         partitions = create_partitions(sorted_counts, max_count, page_size, target_ratio)
         num_partitions = len(partitions)
-        # if we're within the max number of partitions, let's just go with it!
-        if (num_partitions <= max_num_partitions):
-            iterations_taken = i + 1
-            break
-        # otherwise, increment the target ratio
-        target_ratio += step
+        new_target = None
+        # if we still have too many partitions, increase target ratio
+        if (num_partitions > max_num_partitions):
+            print("increase")
+            lower_bound = target_ratio
+            if upper_bound is not None:
+                new_target = target_ratio + ((upper_bound - target_ratio) / 2.0)
+            else:
+                new_target = target_ratio * 2
+            delta = new_target - target_ratio
+        # if we're within the right number of partitions, search for best ratio
+        else:
+            print("decrease")
+            best_ratio = target_ratio
+            upper_bound = target_ratio
+            new_target = target_ratio - ((target_ratio - lower_bound) / 2.0)
+            delta = target_ratio - new_target
+        # if we're barely moving, break    
+        if delta <= threshold:
+                break
+        target_ratio = new_target
+
 
     print("final ratio: ")
     print(target_ratio)
@@ -123,23 +149,29 @@ def compute_minimum_frequency(trace, page_size, max_num_partitions, min_target_r
     print("final partitions: ")
     print(num_partitions)
 
+    print("best ratio: ")
+    print(best_ratio)
+
+    print("final delta: ")
+    print(delta)
+
     print("iterations taken")
     print(iterations_taken)
 
-    return target_ratio
+    return target_ratio # should compute and return real ratio!
 
 if __name__ == "__main__":
     trace_path = DEFAULT_TRACE_PATH
     page_size = page_size.DEFAULT.value
 
-    max_num_partitions = 2
-    initial_target_ratio = 1.5
-    step = 0.5
-    max_iterations = 30
+    max_num_partitions = 3.0
+    initial_target_ratio = 2.0
+    threshold = 0.1
+    max_iterations = 10
 
     if (len(sys.argv) == 2):
         trace_path = sys.argv[1]
 
     trace = pandas.read_csv(trace_path)
     # compute_sector_partitions(trace, page_size)
-    compute_minimum_frequency(trace, page_size, max_num_partitions, initial_target_ratio, step, max_iterations)
+    compute_minimum_frequency(trace, page_size, max_num_partitions, initial_target_ratio, threshold, max_iterations)
